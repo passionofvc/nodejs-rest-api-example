d5000ec315fc33fb5930cbd543f7c882cbc901c2
yamada.kinya <yamada.kinya@growtech.co.jp>
2023-05-03 17:02:42 +0900

    add api

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-const shiken_kubuns_api = require('./shiken_kubuns_api.json');
-const shiken_days_api = require('./shiken_days_api.json');
+const M_SHIKEN = require('./M_SHIKEN.json');
+const T_SHIKEN_QUESTIONS = require('./T_SHIKEN_QUESTIONS.json');
+const T_QUESTION_DETAIL = require('./T_QUESTION_DETAIL.json');
+const T_ANSWER_DETAIL = require('./T_ANSWER_DETAIL.json');
 
 app.set("port", process.env.PORT || 5000);
 
@@ -37,31 +39,67 @@ app.get('/health', function (req, res) {
   res.json({ status: "OK" });
 });
 
-app.get('/api/shiken_kubuns', function (req, res) {
+app.get('/api/get_shiken_kubuns', function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  const kubuns = shiken_kubuns_api.map( (data)=> data.shiken_kubun );
-  res.end( JSON.stringify(kubuns) );
+  const shiken_kubuns = M_SHIKEN.map( (data)=> data.shiken_kubun );
+  res.end( JSON.stringify(shiken_kubuns) );
 })
 
-app.get('/api/shiken_days', function (req, res) {
+app.get('/api/get_shiken_days', function (req, res) {
   const shiken_kubun = req.query.shiken_kubun;
-  const data = shiken_days_api.filter( data =>  data.shiken_kubun == shiken_kubun );
-  const shiken_days = data.map(  (data)=> data.shiken_day );
+  const data = T_SHIKEN_QUESTIONS.filter( data =>  data.shiken_kubun == shiken_kubun );
+  const shiken_wareki = data.map(  (data)=> data.shiken_wareki );
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end( JSON.stringify( uniq(shiken_days) ) );
+  res.end( JSON.stringify( uniq(shiken_wareki) ) );
 })
 
-app.get('/api/day_kubuns', function (req, res) {
+app.get('/api/get_shiken_day_kubuns', function (req, res) {
   const shiken_kubun = req.query.shiken_kubun;
   const shiken_day = req.query.shiken_day;
 
   console.dir(req.query);
-  //console.dir(shiken_days_api);
-  const data = shiken_days_api.filter( (data) =>  (data.shiken_kubun == shiken_kubun && data.shiken_day == shiken_day) );
-  console.log(data);
+  const data = T_SHIKEN_QUESTIONS.filter( (data) =>  (data.shiken_kubun == shiken_kubun && data.shiken_day == shiken_day) );
   const day_kubuns = data.map(  (data)=> data.day_kubun );
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end( JSON.stringify( uniq(day_kubuns) ) );
+})
+
+app.get('/api/get_one_question', function (req, res) {
+  const shiken_kubun = req.query.shiken_kubun;
+  const shiken_day = req.query.shiken_day;
+  const day_kubun = req.query.day_kubun;
+  const que_no = req.query.que_no;
+  console.dir(req.query);
+  const filterQue = T_SHIKEN_QUESTIONS.filter( (data) =>  (
+         data.shiken_kubun == shiken_kubun
+      && data.shiken_day == shiken_day
+      && data.day_kubun == day_kubun
+      && data.que_no == que_no
+      )
+  )[0];
+
+  const questionInfo = {...filterQue, question :[],  answer:[] };
+  const question = T_QUESTION_DETAIL.filter( (data) =>  (
+          data.shiken_kubun == shiken_kubun
+          && data.shiken_day == shiken_day
+          && data.day_kubun == day_kubun
+          && data.que_no == que_no
+      )
+  );
+  question.map( (e) =>
+      questionInfo.question.push({ "que_line_no": e.que_line_no , "que_line_attr" : e.que_line_attr, "que_line_contents":  e.que_line_contents}));
+
+  const answer = T_ANSWER_DETAIL.filter( (data) =>  (
+          data.shiken_kubun == shiken_kubun
+          && data.shiken_day == shiken_day
+          && data.day_kubun == day_kubun
+          && data.que_no == que_no
+      )
+  );
+  console.log(answer);
+  answer.map( (e) => questionInfo.answer.push({ "ans_no" : e.ans_no , "ans_attr" : e.ans_attr, "ans_contents" : e.ans_contents}) );
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end( JSON.stringify(questionInfo) );
 })
 
 
--- test_curl.sh
+++ test_curl.sh
@@ -1,7 +1,9 @@
-#curl -s http://localhost:5000/api/shiken_kubuns | jq .
-#curl -s "http://localhost:5000/api/shiken_days?shiken_kubun=AP" | jq .
+#curl -s http://localhost:5000/api/get_shiken_kubuns | jq .
+#curl -s "http://localhost:5000/api/get_shiken_days?shiken_kubun=AP" | jq .
 
-shiken_day=$(echo "令和5年春期" | nkf -WMQ | tr = %)
-echo ${shiken_day}
+#shiken_day=$(echo "令和5年春期" | nkf -WMQ | tr = %)
+#echo ${shiken_day}
+curl -s "http://localhost:5000/api/get_shiken_day_kubuns?shiken_kubun=AP&shiken_day=2023/04/16" | jq .
 
-curl -s "http://localhost:5000/api/day_kubuns?shiken_kubun=AP&shiken_day=${shiken_day}" | jq .
+day_kubun=$(echo "午前" | nkf -WMQ | tr = %)
+curl -s "http://localhost:5000/api/get_one_question?shiken_kubun=AP&shiken_day=2023/04/16&day_kubun=${day_kubun}&que_no=1" | jq .
